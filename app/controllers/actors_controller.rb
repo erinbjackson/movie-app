bf165a3562279e56505f442fcb0c8d6d5ca804b3
Erin Jackson <erinebjackson@gmail.com>
2022-01-12 16:04:47 -0800

    Adds Movie App

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,8 @@
+class ActorsController < ApplicationController
+  def single_actor
+    actor = params[:id].to_i
+    actor_id = Actor.find_by(id: actor)
+    render json:actor_id.as_json
+    # render json:{message:"Sanity"}
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+# Actor.create(first_name: "Melissa", last_name: "McCarthy", known_for: "bridesmaids" )
+# Actor.create(first_name:"Kristen", last_name: "Wigg", known_for: "SNL")
+# require 'ffaker'
+
+# Actor.create(first_name: FFaker::Name.first_name, last_name:FFaker::Name.last_name, known_for: FFaker::Movie.title)
+# end
