3543e17c61cc9d113f305818b1d8988f11f316e1
Erin Jackson <erinebjackson@gmail.com>
2022-01-15 21:14:33 -0800

    Commits fridays homework for Movie App

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,9 +9,27 @@ class MoviesController < ApplicationController
     movie_id = Movie.find_by(id: movie)
     render json:movie_id
   end
+  def all_actors
+    actors = Actor.all
+    render json: actors.as_json
+  end
   def reverse_order
-    movie_or = Movie.order('title ASC').reverse_order
+    movie_or = Movie.order('title DESC').reverse_order
     render json: movie_or.as_json 
-    
+  end
+  def show
+    name = params[:name]
+    actor = Actor.find_by(first_name: name)
+    render json: {message: actor }
+  end
+  def actor_show
+    name = params["last_name"]
+    actor = Actor.find_by(last_name: name)
+    render json: {message: actor}    
+  end
+  def single_actor
+    name = params[:name]
+    actor = Actor.find_by(first_name: name)
+    render json: {message: actor}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get "/single_actor/:id" => "actors#single_actor"
   get "/all_movies/" => "movies#all_movies"
   get "/single_movie/:id" => "movies#one_movie"
+  get "/all_actors" => "movies#all_actors"
   get "/reverse" => "movies#reverse_order"
+  get "/actor/:name" => "movies#show"
+  get "/actor" => "movies#actor_show"
+  post "/single_actor" => "movies#single_actor"
 end
